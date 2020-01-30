6eaa8908455aa3e5136eb00f9313bc43b76177ca
Arnaldo Morais <arma@cin.ufpe.br>
2020-01-30 20:08:07 -0300

    Update textarea - remove border and increase parent div size

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 
-document.getElementsByClassName('btn-wrap__btn')[0].onclick = clickme;
+document.getElementsByClassName('btn-wrap__btn')[0].onclick = getTextareaValue;
 
-function clickme(){
+function getTextareaValue(){
     const text = document.getElementsByTagName("textarea")[0].value;
-    console.log(text);
+
+    // Separar em 3 partes (jwt) , splitted by dot '.'
+    // Base64 Decode (via atob() function )
+    // Mostrar em forma de JSON
+
+    console.log(atob(text.split('.')[2]));
+}
+
+function isEachFileAProgram(){
+    const answer = "For JS, Yes, this prevents one file interrupt the whole program => Error handler"
+    return answer;   
+}
+
+function nextEpisode(){
+    const answer = "Functions, CH2, YOU DON'T KNOW JS"
 }
