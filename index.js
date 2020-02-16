78ebb733379a8e49c68f6fafd83d5f7b91841e77
Arnaldo Morais <arma@cin.ufpe.br>
2020-02-16 02:31:38 -0300

    Update signature code div

--- index.js
+++ index.js
@@ -1,21 +1,48 @@
 const signatureState = document.querySelector("div.share__text");
+const textArea = document.getElementsByTagName("textarea")[0];
+const validateBtn = document.getElementsByClassName('btn-wrap__btn')[0];
+const algorithmSpan = document.getElementById('js-jwt-security__span');
+const algorithmSelector = document.getElementsByTagName('select')[0];
+const jwtSecurityCodeDiv = document.getElementsByClassName('jwt-security__code')[0];
 
-document.getElementsByClassName('btn-wrap__btn')[0].onclick = getTextareaValue;
+window.onload = function(){
+    updateAlgorithmSpan();
+    let algorithmText = `(<br>
+        base64UrlEncode(header) + "." + 
+        base64UrlEncode(payload),
+    )`;
+    // jwtSecurityCodeDiv.innerHTML = algorithmSpan.innerHTML +  algorithmText;
+
+};
+
+algorithmSelector.onchange = function() {
+    updateAlgorithmSpan();
+}
+
+validateBtn.onclick = getTextareaValue;
+
+function updateAlgorithmSpan() {
+    algorithmSpan.innerText = algorithmSelector.value;
+}
 
 function getTextareaValue(){
-    const text = document.getElementsByTagName("textarea")[0].value;
-
+    let text = textArea.value;
     // Separar em 3 partes (jwt) , splitted by dot '.'
     // Base64 Decode (via atob() function )
     // Mostrar em forma de JSON
     
-    if(!isTokenValid(text)){
-        //modify class
-        signatureState.className = "share__text js-share__text--wrong";
-        signatureState.innerText = "Invalid Signature"
-    }
+    changeSignatureState(text);
+}
 
-    console.log(text.split('.'));
+function changeSignatureState(text) {
+    if (!isTokenValid(text)) {
+        signatureState.className = "share__text js-share__text--invalid";
+        signatureState.innerText = "Invalid Signature";
+    }
+    else {
+        signatureState.className = "share__text js-share__text--correct";
+        signatureState.innerText = "Signature Verified";
+    }
 }
 
 function isTokenValid(token){
