36758e8d1532d898f051255556232ea9c55741be
Arnaldo Morais <arma@cin.ufpe.br>
2020-02-21 00:31:16 -0300

    Refact code into TokenManager - reponsible for managar the jwt token

---
+++ TokenManager.js
@@ -0,0 +1,64 @@
+function getTokenObject(token){
+    let splittedToken = token.split('.');
+    let obj = {
+        header : JSON.parse(atob(splittedToken[0])),
+        payload : JSON.parse(atob(splittedToken[1])),
+        security : splittedToken[2],
+    };
+    return obj;
+}
+
+function isTokenValid(token){
+    const splittedToken = token.split('.');
+    return hasBase64Format(splittedToken) 
+        && isWellFormated(splittedToken);
+}
+
+function isWellFormated(splittedToken){
+    if(splittedToken.length != 3){
+        return false;
+    }
+    return true;
+}
+
+function hasBase64Format(splittedToken){
+    let result = true;
+    try { 
+        atob(splittedToken[0]);
+        atob(splittedToken[1]);
+    } catch (error) {
+        result = false;
+    }
+    return result;
+}
+
+
+function isSignatureValid(token,secret){
+    const splittedToken = token.split('.');
+    let base64Signature = toBase64(splittedToken[2]);
+    let hash = CryptoJS.HmacSHA256(splittedToken[0] + '.' + splittedToken[1], secret);
+    let base64Hash = CryptoJS.enc.Base64.stringify(hash);
+    return base64Signature === base64Hash;
+}
+
+/**
+ * Converts Base64UrlEncoded into Base64Encoded
+ * @param {*} base64url 
+ */
+function toBase64(base64url) {
+    // Replace non-url compatible chars with base64 standard chars
+    base64url = base64url
+        .replace(/-/g, '+')
+        .replace(/_/g, '/');
+
+    // Pad out with standard base64 required padding characters
+    var pad = base64url.length % 4;
+    if(pad) {
+      if(pad === 1) {
+        throw new Error('InvalidLengthError: Input base64url string is the wrong length to determine padding');
+      }
+      base64url += new Array(5-pad).join('=');
+    }
+
+    return base64url;
+}
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const algorithmSelector = document.getElementsByTagName('select')[0];
 const jwtHeaderCodeDiv = document.getElementsByClassName('jwt-header__code')[0];
 const jwtPayloadCodeDiv = document.getElementsByClassName('jwt-payload__code')[0];
 const jwtSecurityCodeDiv = document.getElementsByClassName('jwt-security__code')[0];
+const jwtSecret = document.getElementById('js-json-security__secret');
 
 window.onload = function(){
     updateAlgorithmSpan();
@@ -13,35 +14,26 @@ window.onload = function(){
 
 algorithmSelector.onchange = function() {
     updateAlgorithmSpan();
-}
+};
 
 validateBtn.onclick = function(){
     let token = textArea.value;
-    changeSignatureState(token);
-    let tokenObject = getTokenObject(token);
-    updateCodeDiv(jwtHeaderCodeDiv,tokenObject.header);
-    updateCodeDiv(jwtPayloadCodeDiv,tokenObject.payload);
+    let secret = jwtSecret.value;
+    if(isTokenValid(token)){
+        changeSignatureState(token,secret);
+        let tokenObject = getTokenObject(token);
+        updateCodeDiv(jwtHeaderCodeDiv,tokenObject.header);
+        updateCodeDiv(jwtPayloadCodeDiv,tokenObject.payload);
+    }
+    else{
+        signatureState.className = "share__text js-share__text--invalid";
+        signatureState.innerText = "Invalid Token";
+    }
 };
 
-function getTokenObject(token){
-    let splittedToken = token.split('.');
-    let obj = {
-        header : JSON.parse(atob(splittedToken[0])),
-        payload : JSON.parse(atob(splittedToken[1])),
-        security : splittedToken[2],
-    };
-    return obj;
-}
-function updateAlgorithmSpan() {
-    algorithmSpan.innerText = algorithmSelector.value;
-}
 
-function updateCodeDiv(div, code){
-    div.innerHTML = JSON.stringify(code,undefined, 4);
-}
-
-function changeSignatureState(token) {
-    if (!isTokenValid(token)) {
+function changeSignatureState(token,secret) {
+    if (!isSignatureValid(token,secret)) {
         signatureState.className = "share__text js-share__text--invalid";
         signatureState.innerText = "Invalid Signature";
     }
@@ -51,26 +43,11 @@ function changeSignatureState(token) {
     }
 }
 
-function isTokenValid(token){
-    const splittedToken = token.split('.');
-    return hasBase64Format(splittedToken) && isWellFormated(splittedToken);
+function updateAlgorithmSpan() {
+    algorithmSpan.innerText = algorithmSelector.value;
 }
 
-function isWellFormated(splittedToken){
-    let result = true;
-    if(splittedToken.length != 3){
-        result = false;
-    }
-    return result;
+function updateCodeDiv(div, code){
+    div.innerHTML = JSON.stringify(code,undefined, 4);
 }
 
-function hasBase64Format(splittedToken){
-    let result = true;
-    try { 
-        atob(splittedToken[0]);
-        atob(splittedToken[1]);
-    } catch (error) {
-        result = false;
-    }
-    return result;
-}
