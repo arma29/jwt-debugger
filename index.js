aa80e64572ec7ef869598e9c3f9e2a4598dd01bd
Arnaldo Morais <arma@cin.ufpe.br>
2020-03-02 02:43:22 -0300

    Fix responsive issues

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const jwtHeaderCodeDiv = document.getElementsByClassName('jwt-header__code')[0];
 const jwtPayloadCodeDiv = document.getElementsByClassName('jwt-payload__code')[0];
 const jwtSecurityCodeDiv = document.getElementsByClassName('jwt-security__code')[0];
 const jwtSecret = document.getElementById('js-json-security__secret');
-const encodedDiv = document.getElementsByClassName('comm-process__title')[0];
-const decodedDiv = document.getElementsByClassName('comm-process__title')[1];
+const encodedCommDiv = document.getElementsByClassName('comm-process__title')[0];
+const decodedCommDiv = document.getElementsByClassName('comm-process__title')[1];
+const encodedBoxDiv = document.getElementsByClassName('box-content')[0];
+const decodedBoxDiv = document.getElementsByClassName('box-content')[1];
 const mediaQuery = window.matchMedia('(max-width:768px)');
 
 window.onload = function(){
@@ -19,27 +21,39 @@ algorithmSelector.onchange = function() {
     updateAlgorithmSpan();
 };
 
-function myFunction(){
+function handleEncodedCommDiv(){
+    encodedCommDiv.className = 'comm-process__title js-comm-process--current';
+    decodedCommDiv.className = 'comm-process__title';
+    // TODO: Show and Hide content
+    encodedBoxDiv.className = 'box-content  js-box-content--current';
+    decodedBoxDiv.className = 'box-content'
+}
+
+function handleDecodedCommDiv(){
+    encodedCommDiv.className = 'comm-process__title';
+    decodedCommDiv.className = 'comm-process__title js-comm-process--current';
+    // TODO: Show and Hide content
+    encodedBoxDiv.className = 'box-content';
+    decodedBoxDiv.className = 'box-content  js-box-content--current'
+}
+function handleMediaQuery(){
     if(mediaQuery.matches){
-        encodedDiv.className = 'comm-process__title js-comm-process--current';
-        encodedDiv.firstElementChild.addEventListener('click', function(){
-            encodedDiv.className = 'comm-process__title js-comm-process--current';
-            decodedDiv.className = 'comm-process__title';
-            // TODO: Show and Hide content
-        });
-        decodedDiv.firstElementChild.addEventListener('click',function(){
-            encodedDiv.className = 'comm-process__title';
-            decodedDiv.className = 'comm-process__title js-comm-process--current';
-            // TODO: Show and Hide content
-        });
+        encodedCommDiv.className = 'comm-process__title js-comm-process--current';
+        encodedBoxDiv.className = 'box-content textarea-container js-box-content--current';
+        encodedCommDiv.firstElementChild.addEventListener('click',handleEncodedCommDiv);
+        decodedCommDiv.firstElementChild.addEventListener('click',handleDecodedCommDiv);
     }
     else{
-        encodedDiv.className = 'comm-process__title';
-        decodedDiv.className = 'comm-process__title';
+        encodedCommDiv.className = 'comm-process__title';
+        decodedCommDiv.className = 'comm-process__title';
+        encodedBoxDiv.className = 'box-content';
+        decodedBoxDiv.className = 'box-content';
+        encodedCommDiv.firstElementChild.removeEventListener('click',handleEncodedCommDiv);
+        decodedCommDiv.firstElementChild.removeEventListener('click',handleDecodedCommDiv);
     }
 }
 
-mediaQuery.addEventListener('change', myFunction);
+mediaQuery.addEventListener('change', handleMediaQuery);
 
 validateBtn.onclick = function(){
     let token = textArea.value;
